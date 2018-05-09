5af7bcba1919cbd90f114f93dfdafd8e13f18b04
Sam W <chicagomike999@gmail.com>
2018-05-09 18:04:50 -0400

    Handles some dates
    I forgot I actually need to know peoples birthdays and all that to check them. So I'll do some research and attempt to add some real dates next.

--- src/TextComposer/sobotComposer.py
+++ src/TextComposer/sobotComposer.py
@@ -8,7 +8,7 @@ Other pieces later may add in auto sending or alerts prompting confirmation.
 '''
 import datetime
 now = datetime.datetime.now()
-importantDates = ["4.1", "4.20", "5.1",]
+importantDates = ["4.1", "4.20", "5.1"]
 sobotOutput = open("SobotSays.txt","w+")
 
 # creates the last "from, me" line
@@ -22,16 +22,18 @@ uses some stereotypes to determine keywords used by the person.
  and uses the date to determine what the occasion is. 
  then we jumble that into something human-ish.
 '''
-def celebrationBody(yourPersonality):
-    FoundIt = False
-    for date in importantDates:
-        if(str(now.month) + "." + str(now.day) == date):
-            sobotOutput.write("Good work on that thing you did. I'm glad that think happened. Hope you are doing well.")
-            FoundIt =  True
-            break
-    if (FoundIt == False):
+def celebrationBody():
+    if(str(now.month) + "." + str(now.day) == "1.30"): # Jacob Birthday
+        sobotOutput.write("Happy birthday. I hope its a good one.")
+    if(str(now.month) + "." + str(now.day) == "5.1"): # Sam Birthday
+        sobotOutput.write("Happy birthday. I hope its a good one.")
+    if(str(now.month) + "." + str(now.day) == "10.31"): # Halloween
+        sobotOutput.write("Happy Halloween. I hope you are well. ")
+    if(str(now.month) + "." + str(now.day) == "12.25"): # Christmas
+        sobotOutput.write("Merry Christmas. I hope you are well. ")
+    else:
         sobotOutput.write("I don't know why I'm writing you, but GOOD TO KNOW YA!")
 
 greeting("EXAMPLE_NAME") 
-celebrationBody("relaxed")
+celebrationBody()
 goodbyeMessage("Sam")
